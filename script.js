be4fd1b7d2cd5f586c02a736ed2047c20db3f3ec
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-25 19:30:14 +1100

    Add mathFunction with logic to return result

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function mathFunction(operator,num1,num2) {
+    let result;
+    switch(operator) {
+        case 'plus': result = num1+num2; break;
+        case 'minus': result = num1-num2; break;
+        case 'multiply': result = num1*num2; break;
+        case 'divide': result = num1/num2; break;
+        default: result="ERROR"; break;
+    }
+    return result;
+}
+
