b16b8564c738033b566f93c129d9a7dd0061a56e
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-25 20:29:02 +1100

    Add utility functions and IDs to DOM elements

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const numberButtons = document.querySelectorAll(".num-button");
+const operatorButtons = document.querySelectorAll(".operator-button")
+const clearButton = document.querySelector("#clear-btn")
+const userScreen = document.querySelector("#user-box")
+let num1 = 0;
+let num2 = 0;
+let operator = null;
+let currentInput = ""
 
 
-function operate(operator,num1,num2) {
+
+function clearState() {
+    let num1 = 0;
+    let num2 = 0;
+    let operator = null;
+    userScreen.textContent = "";
+}
+
+function operation(num1,operator,num2) {
     let result;
     switch(operator) {
         case 'plus': result = num1+num2; break;
@@ -12,3 +28,17 @@ function operate(operator,num1,num2) {
     return result;
 }
 
+function logButtonId(button) {
+    console.log(button.target.id)
+}
+
+
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", logButtonId)
+})
+operatorButtons.forEach(button => {
+    button.addEventListener("click", logButtonId)
+})
+
+clearButton.addEventListener("click", clearState)
